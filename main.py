f47b1cda3026f7fe22be8b68afe15e879dd981c9
neeryks <akashdeep.singh49@gmail.com>
2023-02-08 12:31:16 +0530

    Blog Support

--- main.py
+++ main.py
@@ -25,13 +25,37 @@ async def on_message(message):
 
     elif message.content.startswith(".c"):
         data = Responder()
-        ndat = data.code_complete(f"{message.content}")
+        await message.channel.send("From Codex...")
+        ndat = data.code_complete(f"{message.content[2::]}")
         await message.channel.send(f'{ndat.choices[0].text}')
 
-    elif message.content.startswith(''):
+    elif message.content.startswith('.t'):
         data = Responder()
-        ndat = data.text_complete(f"{message.content}")
+        ndat = data.text_complete(f"{message.content[2::]}")
         await message.channel.send(f'{ndat.choices[0].text}')
+    
+    elif message.content.startswith('.blog'):
+        data = Responder()
+        meta_title = data.text_complete(f"meta title on {message.content[6::]}").choices[0].text
+        cont_outline = data.text_complete(f"blog outlne and idea on {meta_title}").choices[0].text
+        print(cont_outline)
+        cont_list = list(cont_outline.split("\n"))
+        with open(f"{meta_title}.txt","a+") as blog_file:
+            blog_file.write(f"<h1>{cont_outline}</h1>\n")
+            for li in cont_list:
+                section = data.text_complete(f"blog section on {li}").choices[0].text
+                blog_file.write(f"<h2>{li}</h2>\n")
+                blog_file.write(f"{section}\n")
+            textfile = discord.File(blog_file)
+            await message.channel.send(file=textfile)
+
+
+
+
+        listdata = list(ndat.split("\n"))
+        
+        
+
 
 
 
